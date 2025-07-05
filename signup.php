3c2b2ea0625ea3235a8012899829ab5665f14497
Shintaro Fujioka <flipper11041006@yahoo.co.jp>
2025-07-05 23:55:27 +0900

    refactor:エラ〜メッセージ変数名変更

--- signup.php
+++ signup.php
@@ -21,6 +21,11 @@ require('function.php');
 debug('「「「「「「「「「「「「「「「「「「「「「「「「「');
 debug('「ユーザー機能画面処理開始');
 debug('「「「「「「「「「「「「「「「「「「「「「「「「「');
+debugLogStart();
+
+//=====================================
+//画面処理開始
+//=====================================
 
 //POST送信
 if(!empty($_POST)){
@@ -30,33 +35,32 @@ if(!empty($_POST)){
   validRequired($_POST['pass'],'pass');
   validRequired($_POST['pass_re'],'pass_re');
 
-  if(empty($err_flg)){
+  if(empty($err_msg)){
     //変数に格納
     $email = $_POST['email'];
     $pass = $_POST['pass'];
     $pass_re = $_POST['pass_re'];
-    debug('passの中身1:'.$pass);
 
     //emailの形式、最大文字数、emailの重複
     validEmail($email,'email');
-    if(empty($err_flg)){
+    if(empty($err_msg)){
       validMaxLen($email,'email');
     }
-    if(empty($err_flg)){
+    if(empty($err_msg)){
       validMailDup($email,'email');
     }
     //passの最小文字数、半角英数字＋きごう、最大文字数
     validPass($pass,'pass');
-    if(empty($err_flg)){
+    if(empty($err_msg)){
       validMaxLen($pass,'pass');
     }
-    if(empty($err_flg)){
+    if(empty($err_msg)){
       validMinLen($pass,'pass');
     }
-    if(empty($err_flg)){
+    if(empty($err_msg)){
       validMatch($pass,$pass_re,'pass');
     }
-    if(empty($err_flg)){
+    if(empty($err_msg)){
       debug('バリデーションOK');
       //例外処理
       try{
@@ -74,7 +78,6 @@ if(!empty($_POST)){
         //クエリ実行
         $stmt = queryPost($dbh,$sql,$data);
         if($stmt){
-          session_start();
           //ユーザーID
           $_SESSION['user_id'] = $dbh->lastInsertId();
           //ログイン日時、ログインリミット
@@ -86,7 +89,7 @@ if(!empty($_POST)){
         }
       }catch(Exception $e){
         debug('エラー発生:'.$e->getMessage());
-        $err_flg['common'] = MSG07;
+        $err_msg['common'] = MSG07;
       }
     }
 
@@ -104,23 +107,15 @@ require('head.php');
 ?>
 <body>
   <!--ヘッダー-->
-  <header class="header">
-    <div class="container">
-      <h1><a href="index.html">いぬの駅</a></h1>
-      <nav>
-        <ul>
-          <li><a href="signup.html">ユーザー登録</a></li>
-          <li><a href="login.html">ログイン</a></li>
-        </ul>
-      </nav>
-    </div>
-  </header>
+  <?php
+  require('header.php'); 
+  ?>
 
   <div class="site-width">
    <div class="one-columns-site">
     <h1 class="title">ユーザー登録</h1>
     <div class="area-msg">
-    <?php if(!empty($err_flg['common'])) echo $err_flg['common']; ?>
+    <?php if(!empty($err_msg['common'])) echo $err_msg['common']; ?>
     </div>
     <form class="simple-form" method="post">
       <label class="">
@@ -128,28 +123,28 @@ require('head.php');
         <input type="text" name="email" class="js-valid-text js-valid-email" value="<?php if(!empty($_POST['email'])) echo $_POST['email']; ?>" placeholder="inu@gmail.com">
       </label>
       <div class="area-msg">
-      <?php if(!empty($err_flg['email'])) echo $err_flg['email']; ?>
+      <?php if(!empty($err_msg['email'])) echo $err_msg['email']; ?>
       </div>
       <label class="">
         パスワード<span class="lab-asterisk">* 半角英数字のみ</span>
         <input type="password" name="pass" class="js-valid-text js-valid-pass" value="<?php if(!empty($_POST['pass'])) echo $_POST['pass']; ?>" placeholder="inuinuinu">
       </label>
       <div class="area-msg">
-        <?php if(!empty($err_flg['pass'])) echo $err_flg['pass']; ?>
+        <?php if(!empty($err_msg['pass'])) echo $err_msg['pass']; ?>
       </div>
       <label class="">
         パスワード(再入力)<span class="lab-asterisk">*</span>
         <input type="password" name="pass_re" class="js-valid-text js-valid-pass-re" value="<?php if(!empty($_POST['pass_re'])) echo $_POST['pass_re']; ?>" placeholder="inuinuinu">
       </label>
       <div class="area-msg">
-      <?php if(!empty($err_flg['pass_re'])) echo $err_flg['pass_re']; ?>
+      <?php if(!empty($err_msg['pass_re'])) echo $err_msg['pass_re']; ?>
       </div>
       <input type="submit" name="submit" value="送信" class="simple-btn">
     </form>
     <div class="divider">
       <span>または</span>
     </div>
-    <p class="divider-point"><a href="login.html">登録済みの方はこちら</a></p>
+    <p class="divider-point"><a href="login.php">登録済みの方はこちら</a></p>
    </div>
   </div>
   <!--フッター-->
