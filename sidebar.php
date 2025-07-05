e00774e55a80695950999600b6643100d227e7b8
Shintaro Fujioka <flipper11041006@yahoo.co.jp>
2025-07-05 17:06:50 +0900

    sidebar機能追加

---
+++ sidebar.php
@@ -0,0 +1,11 @@
+<div class="sidebar sidebar-ad">
+      <ul class="mypage-list">
+        <li><a href="hostRegistEdit.html">ホスト登録・編集</a></li>
+        <li><a href="like.html">お気に入り登録一覧</a></li>
+        <li><a href="setting.html">設定</a></li>
+        <li><a href="petDog.html">愛犬登録</a></li>
+        <li><a href="withDraw.html">退会</a></li>
+        <li><a href="adInterview.html">事前面談連絡掲示板</a></li>
+        <li><a href="reservConfirmation.html">予約一覧</a></li>
+      </ul>
+     </div>
