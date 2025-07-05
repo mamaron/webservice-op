7ebeee9bc8ca32bb05d10ae8c709dc637ce82a1a
Shintaro Fujioka <flipper11041006@yahoo.co.jp>
2025-07-05 23:47:48 +0900

    font-awesomeのlink追加

--- head.php
+++ head.php
@@ -5,5 +5,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="style.css">
   <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c&family=Noto+Sans+JP&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
   <title><?php echo $siteTitle; ?> || いぬの駅</title>
 </head>
