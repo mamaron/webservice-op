41ce086c2c6fdb96c68c33c5b32992dc8fedce4a
Shintaro Fujioka <flipper11041006@yahoo.co.jp>
2025-07-06 23:20:29 +0900

    add:バリデーション追加

--- petDog.php
+++ petDog.php
@@ -1,91 +1,117 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="style.css">
-  <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c&family=Noto+Sans+JP&display=swap" rel="stylesheet">
-  <title>愛犬登録 || いぬの駅</title>
-</head>
-<body>
+<?php
+//共通関数
+require('function.php');
+
+debug('「「「「「「「「「「「「「「「「「「「「「「「「「');
+debug('「愛犬登録・編集機能');
+debug('「「「「「「「「「「「「「「「「「「「「「「「「「');
+debugLogStart();
+
+/*
+<愛犬登録・編集>
+・愛犬情報取得展開
+=>情報ある場合、ない場合でフラグ立てる。
+・POST送信
+・未入力バリデ
+・変数にまとめる(画像はここで、アップロード)
+・DBと比べて違う場合はその他バリデーション
+・DB接続
+・フラグ判断で、INSERT or UPDATE
+・mypage遷移
+*/
+
+//自動認証
+require('auth.php');
+
+//======================================== 
+//画面表示処理開始
+//========================================
+
+//愛犬情報取得
+$dbFormData = getDogData($_SESSION['user_id']);
+debug('愛犬情報:'.print_r($dbFormData,true));
+$edit_flg = ($dbFormData) ? 'true' : 'false';
+debug('愛犬フラグ:'.$edit_flg);
+debug('POST情報:'.print_r($_POST,true));
+debug('GET情報:'.print_r($_FILES,true));
+//POST情報
+if(!empty($_POST) && !empty($_FILES)){
+  debug('POST送信及びFILE送信があります。');
+  //未入力バリデーション
+  validRequired($_POST['dog_name'],'dog_name');
+  validRequired($_POST['sex'],'sex');
+  validRequired($_POST['age'],'age');
+  validRequired($_POST['dog_breed'],'dog_breed');
+  validRequired($_FILES['pic'],'pic');
+  if(empty($err_msg)){
+
+  }
+}
+
+?>
+<?php
+$siteTitle = '愛犬登録';
+require('head.php'); 
+?>
   <!--ヘッダー-->
-  <header class="header">
-    <div class="container">
-      <h1><a href="index.html">いぬの駅</a></h1>
-      <nav>
-        <ul>
-          <li><a href="logout.html">ログアウト</a></li>
-          <li><a href="mypage.html">マイページ</a></li>
-        </ul>
-      </nav>
-    </div>
-  </header>
+  <?php
+  require('header.php');
+  ?>
   <div class="site-width">
    <div class="one-columns-site">
-    <h1 class="title">愛犬登録する</h1>
+    <h1 class="title"><?php echo ($edit_flg === 'true') ? '愛犬情報を変更' : '愛犬登録する'; ?> </h1>
     <div class="area-msg">
-
+      <?php if(!empty($err_msg['common'])) echo $err_msg['common']; ?>
     </div>
-    <form class="simple-form" method="post" style="width:50%;">
+    <form class="simple-form" method="post" style="width:50%;" enctype="multipart/form-data">
       <label class="">
         なまえ<span class="lab-asterisk">*</span>
-        <input type="text" name="dog_name" class="js-valid-name js-valid-text" value="" placeholder="ゴン太">
+        <input type="text" name="dog_name" class="js-valid-name js-valid-text" value="<?php echo getFormData('dog_name'); ?>" placeholder="ゴン太">
       </label>
       <div class="area-msg">
-       
+        <?php if(!empty($err_msg['dog_name'])) echo $err_msg['dog_name']; ?>
       </div>
       <label class="">
         性別<span class="lab-asterisk">*</span><br>
         <label for="">
-          <input type="radio" name="sex" value="male">オス
+          <input type="radio" name="sex" value="male" <?php if($_POST['sex'] === 'male' || (!empty($dbFormData['sex']) && $dbFormData['sex'] === 'male')) echo 'checked'; ?>>オス
         </label>
         <label for="">
-          <input type="radio" name="sex" value="female">メス
+          <input type="radio" name="sex" value="female" <?php if($_POST['sex'] === 'female' || (!empty($dbFormData['sex']) && $dbFormData['sex'] === 'female')) echo 'checked'; ?>>メス
         </label>
       </label>
       <div class="area-msg">
-    
+        <?php if(!empty($err_msg['sex'])) echo $err_msg['sex']; ?>
       </div>
       <label class="">
         年齢<span class="lab-asterisk">*</span>
-        <input type="text" name="age" class="js-valid-text js-valid-age" value="" placeholder="3">
+        <input type="text" name="age" class="js-valid-text js-valid-age" value="<?php echo getFormData('age'); ?>" placeholder="3">
       </label>
       <div class="area-msg">
-       
+        <?php if(!empty($err_msg['age'])) echo $err_msg['age']; ?>
       </div>
       <label class="">
         犬種<span class="lab-asterisk">*</span>
-        <input type="text" name="dog_breed" class="js-valid-dog-breed js-valid-text" value="" placeholder="チワワ">
+        <input type="text" name="dog_breed" class="js-valid-dog-breed js-valid-text" value="<?php echo getFormData('dog_breed'); ?>" placeholder="チワワ">
       </label>
       <div class="area-msg">
-
+        <?php if(!empty($err_msg['dog_breed'])) echo $err_msg['dog_breed']; ?>
       </div>
       写真<span class="lab-asterisk">*</span>
       <label class="area-drop js-area-drop">
         ドラッグ&ドロップ
         <input type="hidden" name="MAX_FILE_SIZE" value="3145728">
         <input type="file" name="pic" class="dog-file js-file" value="">
-        <img src="" alt="" class="preview-img js-img">
+        <img src="<?php echo (!empty($dbFormData['pic']['name'])) ? $dbFormData['pic']['name'] : $_FILES['pic']['name']; ?>" alt="" class="preview-img js-img">
       </label>
       <div class="area-msg">
-      
+        <?php if(!empty($err_msg['pic'])) echo $err_msg['pic']; ?>
       </div>
-      <input type="submit" name="submit" value="登録！" class="simple-btn">
+      <input type="submit" name="submit" value="<?php echo ($edit_flg === 'true')?'編集':'登録';  ?>  " class="simple-btn">
     </form>
    </div>
   </div>
     <!--フッター-->
-    <footer class="footer js-footer">
-      <div class="container">
-        <p>Copyright © 2025 いぬの駅. All rights reserved.</p>
-      </div>
-    </footer>
-    <!--JS-->
-    <script
-      src="https://code.jquery.com/jquery-3.7.1.js"
-      integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
-      crossorigin="anonymous">
-    </script>
-    <script src="main.js"></script>
-</body>
-</html>
+    <?php
+    require('footer.php'); 
+    ?>
