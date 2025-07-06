109b825df7f83546f88285cc1c778537619b5209
Shintaro Fujioka <flipper11041006@yahoo.co.jp>
2025-07-06 10:27:39 +0900

    退会機能の完了

--- withDraw.php
+++ withDraw.php
@@ -35,13 +35,42 @@ if(!empty($_POST)){
     debug('トランザクションスタート');
     $dbh->beginTransaction();
     //usersテーブル
-    $sql = 'UPDATE users SET delete_flg = 1 WHERE id = :id';
-    $data = array( ':id' => $_SESSION['user_id']);
-    $stmt = queryPost($dbh,$sql,$data);
-
-    ここから
+    $sql1 = 'UPDATE users SET delete_flg = 1 WHERE id = :id';
+    $data1 = array( ':id' => $_SESSION['user_id']);
+    $stmt1 = queryPost($dbh,$sql1,$data1);
+    $result1 =  $stmt1->rowCount();
+    //pet_dogテーブル
+    $sql2 = 'UPDATE pet_dog SET delete_flg = 1 WHERE user_id = :u_id';
+    $data2 = array(':u_id' => $_SESSION['user_id']);
+    $stmt2 = queryPost($dbh,$sql2,$data2);
+    //likeテーブル
+    $sql3 = 'DELETE FROM `like` WHERE user_id = :u_id';
+    $data3 = array(':u_id' => $_SESSION['user_id']);
+    $stmt3 = queryPost($dbh,$sql3,$data3);
+    //hostテーブル
+    $sql4 = 'UPDATE host SET delete_flg = 1 WHERE user_id = :u_id';
+    $data4 = array(':u_id' => $_SESSION['user_id']);
+    $stmt4 = queryPost($dbh,$sql4,$data4);
+    //boardテーブル
+    $sql5 = 'UPDATE board SET delete_flg = 1 WHERE `owner` = :u_id OR host = :u_id';
+    $data5 = array(':u_id' => $_SESSION['user_id']);
+    $stmt5 = queryPost($dbh,$sql5,$data5);
+    //availabilityテーブル
+    $sql6 = 'UPDATE `availability` SET delete_flg = 1 WHERE host_id = :u_id';
+    $data6 = array(':u_id' => $_SESSION['user_id']);
+    $stmt6 = queryPost($dbh,$sql6,$data6);
 
     $dbh->commit();
+
+    if($result1){
+      debug('ユーザー情報の論理削除完了.TOPページに遷移します。');
+      header("Location:index.php");
+      exit;
+    }else{
+      debug('users情報削除失敗。');
+      $err_msg['common'] = MSG07;
+    }
+
   }catch(Exception $e){
     $dbh->rollback();
     debug('エラー発生:'.$e->getMessage());
@@ -52,6 +81,7 @@ debug('画面実装処理終了<<<<<<<<<<<<<<<<<<<<<<<<<<<<<');
 ?>
 
 <?php 
+$siteTitle = '退会';
 require('head.php'); 
 ?>
   <!--ヘッダー-->
@@ -63,6 +93,7 @@ require('head.php');
    <div class="one-columns-site">
     <h1 class="title">退会する</h1>
     <div class="area-msg">
+      <?php if(!empty($err_msg['common'])) echo $err_msg['common']; ?>
     </div>
     <p class="passremind_msg">
       寂しいわん
